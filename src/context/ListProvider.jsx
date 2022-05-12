b80a5988f86d44c0494162cebadc2985db5830dc
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-12 10:17:49 -0700

    wrote switch case for reducer in context

---
+++ src/context/ListProvider.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useReducer } from "react";
+
+const initialToGets = [{ id: Date.now(), text: 'pickles', done: false }];
+
+const toGetReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TOGET':
+            return [...state, { id: Date.now(), text: action.payload.text, done: false }];
+        case 'UPDATE_TOGET':
+            return state.map((toget) => {
+                if (toget.id === action.payload.toget.id) {
+                    const { done, text } = action.payload.toget;
+                    return { ...toget, text, done };
+                }
+                return toget;
+            });
+        case 'DELETE_TOGET':
+            return state.filter((toget) => toget.id !== action.payload.id);
+        default:
+            throw new Error(`Action type ${action.type} is not supported.`);
+    }
+};
