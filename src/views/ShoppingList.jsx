23f25c8bc7b87ad436453c8aaff288904910efa0
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-12 11:15:23 -0700

    set default state in ShoppingList

--- src/views/ShoppingList.jsx
+++ src/views/ShoppingList.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { useState } from 'react';
+import ToGet from '../components/ToGet';
+import { useToGets } from '../context/ListProvider';
 
 export default function ShoppingList() {
+    const [newToGet, setNewToGet] = useState('');
+    const { togets, handleAddToGet, handleUpdateToGet, handleDeleteToGet } = useToGets();
+    
   return (
     <div>ShoppingList</div>
   )
