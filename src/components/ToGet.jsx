1bb90e2b1e5fddf6e4a0261c2ccb7f7106b20bd2
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-13 10:24:41 -0700

    added edit functionality to ToGet component

--- src/components/ToGet.jsx
+++ src/components/ToGet.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react';
 
 export default function ToGet({ toget, onUpdate, onDelete }) {
+    const [isEditing, setIsEditing] = useState(false);
+    
+    let content;
+
+    if (isEditing) {
+        content = (
+            <form onSubmit={(event) => { event.preventDefault(); setIsEditing(false); }} >
+                <input value={toget.text} onChange={(e) => onUpdate({ ...toget, text: e.target.value })}></input>
+                <button>Save</button>  
+            </form>
+        )
+    } else {
+        content = (
+            <button type='button' onClick={() => setIsEditing(true)} >Edit</button>
+        )
+    }
+
   return (
     <>
         <input type='checkbox' checked={toget.done} onChange={(e) => {
-              onUpdate({ ...toget, done: e.target.checked });
-          }} />
-          {toget.text}
+            onUpdate({ ...toget, done: e.target.checked });
+        }} />
+        {toget.text}
+        {content}
         <button type='button' onClick={() => onDelete(toget.id)} >Delete Item</button>
     </>
-  )
+)
 }
